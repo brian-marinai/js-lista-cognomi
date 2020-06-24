66834f7494debc22351a4b6cf5ed7f866b3f7a87
Brian Marinai <brianmarinai.bm@gmail.com>
2020-06-24 15:57:18 +0200

    commit esercizio in svolgimento

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+
+var cognomeUtente = prompt("Inserisci il tuo cognome")
+
+var cognomi = ["Bucciarati", "Mista","Abbacchio","Givanna","Fugo","Doppio" ]
+
+cognomi.push(cognomeUtente);
+
+cognomi.sort();
+
+console.log(cognomi);
