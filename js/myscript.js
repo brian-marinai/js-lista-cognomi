fdda06b62234f8b55b7ead00ef6b061cd80a23ab
Brian Marinai <brianmarinai.bm@gmail.com>
2020-06-24 17:56:27 +0200

    esercizio terminato

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,15 @@
 
 var cognomeUtente = prompt("Inserisci il tuo cognome")
 
-var cognomi = ["Bucciarati", "Mista","Abbacchio","Givanna","Fugo","Doppio" ]
+var cognomi = ["Bucciarati", "Mista","Abbacchio","Givanna","Fugo","Doppio"]
 
 cognomi.push(cognomeUtente);
 
 cognomi.sort();
 
-console.log(cognomi);
+var lista = document.getElementById("lista");
+
+for (var i = 0; i < cognomi.length; i++) {
+  lista.innerHTML += "<li>" + cognomi[i] + "</li>";
+}
+document.getElementById("posizione").innerHTML = "sei alla posizione " + (cognomi.indexOf(cognomeUtente) + 1);
